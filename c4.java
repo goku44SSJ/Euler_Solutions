88a64f00a286cbda66f4a10795891f931927277c
goku44SSJ <119090723+goku44SSJ@users.noreply.github.com>
2022-12-01 19:36:59 +0530

    Euler Problem#4
    Link to the question : https://projecteuler.net/problem=4

---
+++ c4.java
@@ -0,0 +1,45 @@
+/*
+A palindromic number reads the same both ways. 
+The largest palindrome made from the product of two 2-digit numbers is 9009 = 91 × 99.
+Find the largest palindrome made from the product of two 3-digit numbers.  
+*/
+
+import java.lang.*;
+public class c4 
+{
+
+	public static void main(String[] args) 
+	{
+		int c=0,e=0;
+		for(int i=100;i<=999;i++)
+		{
+			for(int j=100;j<=999;j++)
+			{
+				int d=i*j;
+				boolean b=c4.checkPalin(d);
+				if(b==true)
+					e=d;
+				if(c<e)
+					c=e;
+			}		
+
+		}
+		System.out.println("The largest palindrom made from 3 digit numbers are : " +c);
+	}
+	
+	static boolean checkPalin(int n)
+	{
+		int r,t=n,c=0;
+		while (n>0)
+		{
+			r=n%10;
+			c=(c*10)+r;
+			n=n/10;
+		}
+		if(t==c)
+			return true;
+		else
+			return false;
+	}
+
+}
