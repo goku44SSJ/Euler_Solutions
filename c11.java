6291d6a3f787ac0dc0773fc753f4ab81ef8e3315
goku44SSJ <119090723+goku44SSJ@users.noreply.github.com>
2022-12-07 22:13:22 +0530

    Euler Problem #12
    Link to the question : https://projecteuler.net/problem=12

---
+++ c11.java
@@ -0,0 +1,35 @@
+/*
+ * What is the value of the first triangle number to have over five hundred divisors?
+ */
+class c12 
+{
+
+	public static void main(String[] args) 
+	{int c=0;
+		for(int i=1;i>=0;i++)
+		{
+			c=c+i;
+			int b=c12.checkDivisor(c);
+			if(b>500)
+			{
+				System.out.println(c);
+				break;
+			}
+			
+		}
+
+	}
+	
+	static int checkDivisor(int n)
+	{
+		int c=0;
+		for(int i=1; i<=n;i++)
+		{
+			if(n%i==0)
+			{
+				c++;
+			}
+		}return c;
+	}
+
+}
